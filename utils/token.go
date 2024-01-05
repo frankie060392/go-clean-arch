cc8b0e5202e9dcf87a4cc2574946ff45dbfab8b5
Frankie <frankie@gmail.com>
2024-01-05 10:48:34 +0700

    Verify jwt token

--- utils/token.go
+++ utils/token.go
@@ -35,3 +35,34 @@ func CreateToken(ttl time.Duration, payload interface{}, privateKey string) (str
 	}
 	return token, nil
 }
+
+func ValidateToken(token string, publicKey string) (interface{}, error) {
+	decodedPublicKey, err := base64.StdEncoding.DecodeString(publicKey)
+	if err != nil {
+		return nil, fmt.Errorf("could not decode: %w", err)
+	}
+
+	key, err := jwt.ParseRSAPublicKeyFromPEM(decodedPublicKey)
+
+	if err != nil {
+		return "", fmt.Errorf("validate: parse key: %w", err)
+	}
+
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("unexpected method: %s", t.Header["alg"])
+		}
+		return key, nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("validate: %w", err)
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok || !parsedToken.Valid {
+		return nil, fmt.Errorf("validate: invalid token")
+	}
+
+	return claims["sub"], nil
+}
